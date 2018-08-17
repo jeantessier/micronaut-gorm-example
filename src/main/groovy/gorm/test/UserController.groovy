f10920d7534f27a1b3c4097e40a9f7d38a42dc26
Jean Tessier <jean@jeantessier.com>
2018-08-17 16:13:14 -0700

    Add a simple controller to do CRUD
    The controller automatically converts
    objects to JSON.
    
    For some reason, it is not converting
    `allErrors` to text.

---
+++ src/main/groovy/gorm/test/UserController.groovy
@@ -0,0 +1,45 @@
+package gorm.test
+
+import io.micronaut.http.annotation.*
+import io.micronaut.spring.tx.annotation.*
+import java.text.*
+
+@Controller("/users")
+class UserController {
+
+    @Get("/")
+    @Transactional(readOnly = true)
+    def index() {
+        User.list()
+    }
+
+    @Get("/{id}")
+    @Transactional(readOnly = true)
+    def show(long id) {
+        User.get(id)
+    }
+
+    @Post("/")
+    @Transactional
+    def save(String emailAddress, String password, String fullname) {
+        def user = new User(emailAddress: emailAddress, password: password, fullname: fullname)
+        if (user.save()) {
+            return user
+        } else {
+            return user.errors.allErrors.collect { MessageFormat.format(it.defaultMessage, it.arguments) }.join(", ")
+        }
+    }
+
+    @Delete("/{id}")
+    @Transactional
+    def delete(long id) {
+        def user = User.get(id)
+        if (user) {
+            user.delete()
+            return "OK"
+        } else {
+            return "Could not delete user ${id}."
+        }
+    }
+
+}
